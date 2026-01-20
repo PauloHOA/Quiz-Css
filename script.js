b058b55425476d732abde61710930e043888edd9
PauloHOA <108771796+PauloHOA@users.noreply.github.com>
2026-01-20 05:06:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 function verificarResultado() {
   let pontos = 0;
 
+ function verificarResultado() {
+  let pontos = 0;
+
   if (document.getElementById("p1certo").checked) pontos++;
   if (document.getElementById("p2certo").checked) pontos++;
   if (document.getElementById("p3certo").checked) pontos++;
+  if (document.getElementById("p4certo").checked) pontos++;
+  if (document.getElementById("p5certo").checked) pontos++;
+  if (document.getElementById("p6certo").checked) pontos++;
+  if (document.getElementById("p7certo").checked) pontos++;
+  if (document.getElementById("p8certo").checked) pontos++;
+  if (document.getElementById("p9certo").checked) pontos++;
+  if (document.getElementById("p10certo").checked) pontos++;
+  if (document.getElementById("p11certo").checked) pontos++;
+  if (document.getElementById("p12certo").checked) pontos++;
+  if (document.getElementById("p13certo").checked) pontos++;
+  if (document.getElementById("p14certo").checked) pontos++;
+  if (document.getElementById("p15certo").checked) pontos++;
+  if (document.getElementById("p16certo").checked) pontos++;
+  if (document.getElementById("p17certo").checked) pontos++;
+  if (document.getElementById("p18certo").checked) pontos++;
+  if (document.getElementById("p19certo").checked) pontos++;
+  if (document.getElementById("p20certo").checked) pontos++;
+  if (document.getElementById("p21certo").checked) pontos++;
+  if (document.getElementById("p22certo").checked) pontos++;
+  if (document.getElementById("p23certo").checked) pontos++;
+  if (document.getElementById("p24certo").checked) pontos++;
+  if (document.getElementById("p25certo").checked) pontos++;
+  if (document.getElementById("p13certo").checked) pontos++;
+  
 
-  alert("Você acertou " + pontos + " de 3 perguntas!");
+  alert("Você acertou " + pontos + " de 25 perguntas!");
 }
+
+
